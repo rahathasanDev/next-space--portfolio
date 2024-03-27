ce76a11b0ab87d1dfb43b5864aa4088dfacaaf69
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-27 19:19:29 +0600

    Herocontent section added

---
+++ Components/sub/HeroContent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {motion} from 'framer-motion'
+
+const HeroContent = () => {
+  return (
+    <motion.div 
+    >
+      
+    </motion.div>
+  );
+};
+
+export default HeroContent;

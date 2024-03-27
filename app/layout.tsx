cc96354c20eef1685d03d6c8bca073712b00fc78
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-27 20:09:16 +0600

    Constants style added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/Components/main/StarBackground";
+import Navbar from "@/Components/main/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
         <StarsCanvas></StarsCanvas>
+        <Navbar></Navbar>
         {children}
       </body>
     </html>

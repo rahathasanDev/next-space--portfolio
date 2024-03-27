629bbdd1783350b0f7003195ef7283b8ca1e31cc
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-27 19:47:33 +0600

    utilities motion added and global css done

--- Components/main/Hero.tsx
+++ Components/main/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeroContent from '../sub/HeroContent';
 
 const Hero = () => {
   return (
@@ -10,7 +11,7 @@ const Hero = () => {
       className='rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover'>
         <source  src='/blackhole.webm' type='video/webm'/>
       </video>
-      
+      <HeroContent></HeroContent>
     </div>
   );
 };
